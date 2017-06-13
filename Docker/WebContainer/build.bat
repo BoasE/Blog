764e0903729c57c0b44ab06ef3ea40a54a59d56a
Gentlehag <info@it-design.biz>
2017-06-13 08:30:44 +0200

    docker nginx example

--- Docker/WebContainer/build.bat
+++ Docker/WebContainer/build.bat
@@ -1,4 +1,5 @@
 dotnet restore
 dotnet build
 dotnet publish -o:out
-docker build -t webcontainer:0.1 .
+docker build -t webcontainer:0.1 -t boasbls/corehelloworld:0.1 -t eu.gcr.io/gpkubernetes/kubercore:0.1 .
+
